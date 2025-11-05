9f1e266407dda2a41ff1cfe861510cf3f92801c9
Gabriel <gabriel.uraza@correounivalle.edu.co>
2025-11-05 14:16:47 -0500

    clase carta modificada, 20 cartas añadidas

--- yu_gi_oh/src/model/card.py
+++ yu_gi_oh/src/model/card.py
@@ -7,27 +7,14 @@ class Card:
     Represents a single card in the game.
     """
 
-    def __init__(self, name: str, attack: int, defense: int, card_type: str = "normal"):
-        self.name = name
-        self.attack = attack
+    def __init__(self, id: str, name: str, attack: int, defense: int, level: int):
+        self.id = id # 
+        self.name = name # el mago 
+        self.attack = attack # 290
         self.defense = defense
-        self.card_type = card_type
+        self.level = level
 
     def __repr__(self):
         return f"{self.name} (ATK: {self.attack}, DEF: {self.defense})"
 
-    def compare(self, other_card):
-        """
-        Compare this card to another one.
-        Returns:
-        - 1 if this card wins
-        - -1 if this card loses
-        - 0 if it's a tie
-        """
-        if self.attack > other_card.defense:
-            return 1
-        elif self.attack < other_card.defense:
-            return -1
-        else:
-            return 0
 
